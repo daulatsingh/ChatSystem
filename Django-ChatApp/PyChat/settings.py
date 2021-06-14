0485f5159c60f7af367b08d0496a276383e7234a
daulat.kushwaha <daulat.kushwaha@vfirst.com>
2021-06-14 17:57:36 +0530

    db changes

--- Django-ChatApp/PyChat/settings.py
+++ Django-ChatApp/PyChat/settings.py
@@ -77,14 +77,18 @@ WSGI_APPLICATION = 'PyChat.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'test_db',
-        'NAME': 'haptik',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     # 'NAME': 'test_db',
+    #     'NAME': 'haptik',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
